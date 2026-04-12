b172f226f0fffee424dc32db2c521676bedfadda
christina1797406 <a1797406@adelaide.edu.au>
2026-04-12 19:45:41 +0930

    remove preprocessing README.md in /documentations and created preprocess script in /code

---
+++ code/preprocessing.R
@@ -0,0 +1,65 @@
+# Data Preprocessing Script
+
+install.packages(c("tidyverse", "lubridate", "forecast", "tseries", "janitor"))
+
+# Load libraries
+library(tidyverse) # includes readr, dplyr, tidyr
+library(janitor)
+
+# Load data, clean names
+data <- read_csv("data/raw_data/1960-2025.csv",
+                 skip = 9,
+                 n_max = 17,
+                 col_names = TRUE) %>%
+  clean_names()
+
+# Convert columns except 'data_series' to numeric
+data <- data %>%
+  mutate(across(-data_series, as.numeric))
+
+# Reshape data from wide to long format
+data_long <- data %>%
+  pivot_longer(
+    cols = -data_series,
+    names_to = "year",
+    values_to = "value"
+  ) %>%
+  mutate(year = as.numeric(str_remove(year, "x"))) # Remove 'x' prefix
+
+## Print summary to check
+# glimpse(data_long)
+# summary(data_long)
+
+## Check in for missing values
+# sum(is.na(data_long$value)) # 128 NA's
+
+# Total Fertility Rate
+tfr_data <- data_long %>%
+  filter(data_series == "Total Fertility Rate (TFR) (Per Female)") %>%
+  arrange(year) %>%
+  rename(TFR = value)
+
+# Total Live Births
+tlb_data <- data_long %>%
+  filter(data_series == "Total Live-Births (Number)") %>%
+  arrange(year) %>%
+  rename(TLB = value)
+
+# Train-test split
+train_years <- 1960:2012
+test_years  <- 2013:2025
+
+tfr_train <- tfr_data %>% filter(year %in% train_years)
+tfr_test  <- tfr_data %>% filter(year %in% test_years)
+
+tlb_train <- tlb_data %>% filter(year %in% train_years)
+tlb_test  <- tlb_data %>% filter(year %in% test_years)
+
+# Save processed data
+write_csv(tfr_train, "data/processed_data/tfr_train.csv")
+write_csv(tfr_test,  "data/processed_data/tfr_test.csv")
+
+write_csv(tlb_train, "data/processed_data/tlb_train.csv")
+write_csv(tlb_test,  "data/processed_data/tlb_test.csv")
+
+cat("Preprocessing complete.\n")
